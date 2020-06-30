bedbaa5a9710a77b7ffe1150e973463586675927
Alexandre <alexsantsjrr@gmail.com>
2020-06-30 18:47:10 -0300

    news features

--- public/script.js
+++ public/script.js
@@ -1,8 +1,60 @@
+
 const currentPage = location.pathname
 const menuItems = document.querySelectorAll("header .menu a")
 
-for (item of menuItems){
-    if(currentPage.includes(item.getAttribute("href"))){
+for (item of menuItems) {
+    if (currentPage.includes(item.getAttribute("href"))) {
         item.classList.add("active")
     }
 }
+
+//Paginação
+function paginate(selectedPage, totalPages) {
+    let pages = [],
+        oldPage
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+        const firstAndLastePage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+        if (firstAndLastePage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push("...")
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+
+            oldPage = currentPage
+        }
+    }
+    return pages
+}
+
+const pagination = document.querySelector(".pagination")
+const filter = pagination.dataset.filter;
+const page = +pagination.dataset.page;
+const total = +pagination.dataset.total;
+const pages = paginate(page, total)
+
+let elements = ""
+
+for (let page of pages){
+    if(String(page).includes("...")){
+        elements += `<span>${page}</page>`
+    }else {
+        if(filter){
+            elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+        }else {
+            elements += `<a href="?page=${page}">${page}</a>`
+        }
+    }
+}
+
+pagination.innerHTML = elements
